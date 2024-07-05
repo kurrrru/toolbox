088db0e957456028fd577202241944c0b90ce30e
kurrrru <kurrrru8850@gmail.com>
2024-07-06 00:36:12 +0900

    [Add] alignment algorithms

---
+++ toolbox/alignment.h
@@ -0,0 +1,4 @@
+#include "../src/bioinfo/alignment/global_alignment/nw.hpp"
+#include "../src/bioinfo/alignment/global_alignment/nwg.hpp"
+#include "../src/bioinfo/alignment/global_alignment/diff.hpp"
+#include "../src/bioinfo/alignment/global_alignment/wavefront.hpp"
